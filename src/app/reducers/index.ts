53ad4cc318a4b749a1f8ea2244acec0aa489f7b0
Tim <tja2353@gmail.com>
2017-07-15 12:01:24 +0100

    step 1

---
+++ src/app/reducers/index.ts
@@ -0,0 +1,48 @@
+import {
+  ActionReducerMap,
+  createSelector,
+  createFeatureSelector,
+  compose,
+  ActionReducer,
+  combineReducers,
+  Action,
+  ActionReducerFactory,
+} from '@ngrx/store';
+import * as fromRouter from '@ngrx/router-store';
+// import { environment } from '../../environments/environment';
+
+/**
+ * Every reducer module's default export is the reducer function itself. In
+ * addition, each module should export a type or interface that describes
+ * the state of the reducer plus any selector functions. The `* as`
+ * notation packages up all of the exports into a single object.
+ */
+
+import * as fromLayout from '../core/reducers/layout';
+
+/**
+ * As mentioned, we treat each reducer like a table in a database. This means
+ * our top level state interface is just a map of keys to inner state types.
+ */
+export interface State {
+  layout: fromLayout.State;
+}
+
+/**
+ * Our state is composed of a map of action reducer functions.
+ * These reducer functions are called with each dispatched action
+ * and the current or initial state and return a new immutable state.
+ */
+export const reducers: ActionReducerMap<State> = {
+  layout: fromLayout.reducer,
+};
+
+/**
+ * Layout Reducers
+ */
+export const getLayoutState = createFeatureSelector<fromLayout.State>('layout');
+
+export const getShowSidenav = createSelector(
+  getLayoutState,
+  fromLayout.getShowSidenav
+);
