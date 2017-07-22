ef2bc8b574bf4120cc056cf39620aacea0008d2d
Tim <tja2353@gmail.com>
2017-07-22 09:46:32 +0100

    @Input() set

--- src/app/books/components/book-search/book-search.component.ts
+++ src/app/books/components/book-search/book-search.component.ts
@@ -10,7 +10,15 @@ import {
     templateUrl: 'book-search.component.html'
 })
 export class BookSearchComponent {
+    private _searching: boolean = false;
+
     @Input() query = '';
-    @Input() searching = false;
+    // @Input() searching = false;
+    @Input()
+    set searching(searching: boolean) {
+        this._searching = searching;
+        console.log('searching>', this._searching);
+    }
+
     @Output() search = new EventEmitter<string>();
 }
